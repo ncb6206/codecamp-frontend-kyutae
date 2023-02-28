19611caa189919c8a7f704ce3fcd2ade895c8a21
ncb6206 <ncb6206@gmail.com>
2023-02-28 17:01:35 +0900

    section.4 포트폴리오 수정

--- freeboard_frontend/pages/boards/new/index.js
+++ freeboard_frontend/pages/boards/new/index.js
@@ -1,46 +1,80 @@
 import {
   Wrapper,
-  Box,
-  Header,
-  User,
+  WriterWrapper,
   Title,
-  Content,
-  Address,
+  Zipcode,
   Youtube,
-  Picture,
-  Main,
-  SetForm,
+  Password,
+  InputWrapper,
   Writer,
-  WriterInput,
-  PasswordInput,
+  Label,
+  Subject,
+  Contents,
+  ZipCodeWrapper,
+  SearchButton,
+  Address,
+  SubmitButton,
+  ImageWrapper,
+  UploadButton,
+  OptionWrapper,
+  RadioLabel,
+  RadioButton,
+  ButtonWrapper,
 } from "../../../styles/emotion";
 
-export default function BoardPage() {
+export default function BoardWriteUI() {
   // 여기는 자바스크립트 쓰는곳
 
   return (
     <Wrapper>
-      <Box>
-        <Header>게시물 등록</Header>
-        <User>
-          <SetForm>
-            <Writer>작성자</Writer>
-            <WriterInput placeholder="이름을 적어주세요"></WriterInput>
-          </SetForm>
-          <SetForm>
-            <Writer>비밀번호</Writer>
-            <PasswordInput placeholder="비밀번호를 입력해주세요"></PasswordInput>
-          </SetForm>
-        </User>
-        <Title>
-          <SetForm></SetForm>
-        </Title>
-        <Content></Content>
-        <Address></Address>
-        <Youtube></Youtube>
-        <Picture></Picture>
-        <Main></Main>
-      </Box>
+      <Title>게시물 등록</Title>
+      <WriterWrapper>
+        <InputWrapper>
+          <Label>작성자</Label>
+          <Writer type="text" placeholder="이름을 적어주세요" />
+        </InputWrapper>
+        <InputWrapper>
+          <Label>비밀번호</Label>
+          <Password type="password" placeholder="비밀번호를 입력해주세요" />
+        </InputWrapper>
+      </WriterWrapper>
+      <InputWrapper>
+        <Label>제목</Label>
+        <Subject type="text" placeholder="제목을 작성해주세요." />
+      </InputWrapper>
+      <InputWrapper>
+        <Label>내용</Label>
+        <Contents type="text" placeholder="내용을 작성해주세요." />
+      </InputWrapper>
+      <InputWrapper>
+        <Label>주소</Label>
+        <ZipCodeWrapper>
+          <Zipcode placeholder="07250" />
+          <SearchButton>우편번호 검색</SearchButton>
+        </ZipCodeWrapper>
+        <Address />
+        <Address />
+      </InputWrapper>
+      <InputWrapper>
+        <Label>유튜브</Label>
+        <Youtube placeholder="링크를 복사해주세요." />
+      </InputWrapper>
+      <ImageWrapper>
+        <Label>사진 첨부</Label>
+        <UploadButton>+</UploadButton>
+        <UploadButton>+</UploadButton>
+        <UploadButton>+</UploadButton>
+      </ImageWrapper>
+      <OptionWrapper>
+        <Label>메인 설정</Label>
+        <RadioButton type="radio" name="main" value="youtube" id="youtube" checked />
+        <RadioLabel for="youtube">유튜브</RadioLabel>
+        <RadioButton type="radio" name="main" value="picture" id="picture" />
+        <RadioLabel for="picture">사진</RadioLabel>
+      </OptionWrapper>
+      <ButtonWrapper>
+        <SubmitButton>등록하기</SubmitButton>
+      </ButtonWrapper>
     </Wrapper>
   );
 }
--- freeboard_frontend/styles/emotion.js
+++ freeboard_frontend/styles/emotion.js
@@ -4,77 +4,165 @@ export const Wrapper = styled.div`
   * {
     box-sizing: border-box;
   }
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-`;
-
-export const Box = styled.div`
   width: 1200px;
-  background: #ffffff;
-  box-shadow: 0px 4px 20px rgba(0, 0, 0, 0.2);
+  margin: 100px;
+  padding: 80px 102px 100px;
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
-  gap: 20px;
-  padding: 50px;
+  align-items: center;
+  border: none;
+  box-shadow: 0px 0px 10px gray;
 `;
 
-export const Header = styled.div`
-  font-family: "Noto Sans CJK KR";
-  font-style: normal;
-  font-weight: 700;
+export const Title = styled.div`
+  font-family: Arial, Helvetica, sans-serif;
   font-size: 36px;
-  line-height: 53px;
-  text-align: center;
+  font-weight: bold;
 `;
 
-export const SetForm = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-`;
-
-export const User = styled.div`
+export const WriterWrapper = styled.div`
+  width: 100%;
   display: flex;
   flex-direction: row;
-  justify-content: space-around;
+  justify-content: space-between;
+  padding-top: 40px;
 `;
 
-export const Writer = styled.span`
+export const InputWrapper = styled.div`
+  width: 100%;
+  padding-top: 40px;
+`;
+
+export const Writer = styled.input`
+  width: 486px;
+  height: 52px;
+  padding-left: 16px;
+  border: 1px solid #bdbdbd;
+`;
+
+export const Password = styled.input`
+  width: 486px;
+  height: 52px;
+  padding-left: 16px;
+  border: 1px solid #bdbdbd;
+`;
+
+export const Label = styled.div`
+  padding-bottom: 16px;
+  font-size: 16px;
+  font-weight: 500;
+`;
+
+export const Subject = styled.input`
+  width: 996px;
+  height: 52px;
+  padding-left: 16px;
+  border: 1px solid #bdbdbd;
+`;
+
+export const Content = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
+
+export const ContentWord = styled.label`
   font-family: "Noto Sans CJK KR";
   font-style: normal;
   font-weight: 500;
   font-size: 16px;
   line-height: 24px;
-  margin-bottom: 1rem;
 `;
 
-export const WriterInput = styled.input`
-  background: #ffffff;
+export const Contents = styled.textarea`
+  width: 996px;
+  height: 480px;
+  padding: 14px;
+  padding-left: 16px;
   border: 1px solid #bdbdbd;
-  width: 30rem;
-  height: 3.2rem;
 `;
 
-export const PasswordInput = styled.input`
-  background: #ffffff;
-  border: 1px solid #bdbdbd;
-  width: 30rem;
-  height: 3.2rem;
-`;
-
-export const Title = styled.div`
+export const ZipCodeWrapper = styled.div`
   display: flex;
   flex-direction: row;
+  gap: 1rem;
 `;
 
-export const Content = styled.div``;
+export const Zipcode = styled.input`
+  width: 77px;
+  height: 52px;
+  padding-left: 16px;
+  border: 1px solid #bdbdbd;
+`;
 
-export const Address = styled.div``;
+export const SearchButton = styled.button`
+  background: #000000;
+  color: #ffffff;
+  width: 124px;
+  height: 52px;
+  cursor: pointer;
+`;
 
-export const Youtube = styled.div``;
+export const Address = styled.input`
+  width: 996px;
+  height: 52px;
+  margin-top: 16px;
+  padding-left: 16px;
+  border: 1px solid #bdbdbd;
+`;
 
-export const Picture = styled.div``;
+export const Youtube = styled.input`
+  width: 996px;
+  height: 52px;
+  padding-left: 16px;
+  border: 1px solid #bdbdbd;
+`;
 
-export const Main = styled.div``;
+export const ImageWrapper = styled.div`
+  width: 100%;
+  padding-top: 40px;
+`;
+
+export const UploadButton = styled.button`
+  width: 78px;
+  height: 78px;
+  background-color: #bdbdbd;
+  margin-right: 24px;
+  outline: none;
+  border: none;
+  cursor: pointer;
+`;
+
+export const OptionWrapper = styled.div`
+  width: 100%;
+  padding-top: 40px;
+`;
+
+export const RadioButton = styled.input`
+  cursor: pointer;
+`;
+
+export const RadioLabel = styled.label`
+  margin-left: 8px;
+  margin-right: 20px;
+  font-weight: 500;
+  cursor: pointer;
+`;
+
+export const ButtonWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  padding-top: 80px;
+`;
+
+export const SubmitButton = styled.button`
+  background: #ffd600;
+  border: none;
+  padding: 14px 60px;
+  font-size: 16px;
+  font-weight: 500;
+  line-height: 24px;
+  cursor: pointer;
+`;
