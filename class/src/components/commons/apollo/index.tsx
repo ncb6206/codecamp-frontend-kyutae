0230b1e408d91b7960cce81ad1e39160ec62e70f
ncb6206 <ncb6206@gmail.com>
2023-03-17 03:22:25 +0900

    section.14-3 포트폴리오 진행중...

--- class/src/components/commons/apollo/index.tsx
+++ class/src/components/commons/apollo/index.tsx
@@ -1,5 +1,6 @@
 import { ApolloProvider, ApolloClient, InMemoryCache, ApolloLink } from "@apollo/client";
 import { createUploadLink } from "apollo-upload-client";
+import { useEffect } from "react";
 import { useRecoilState } from "recoil";
 import { accessTokenState } from "../../../commons/store";
 
@@ -11,6 +12,40 @@ const GLOBAL_STATE = new InMemoryCache();
 
 export default function ApolloSetting(props: IApolloSettingProps) {
   const [accessToken, setAccessToken] = useRecoilState(accessTokenState);
+
+  // 1. 프리렌더링 예제 - process.browser 방법
+  // if (process.browser) {
+  //   // console.log("지금은 브라우저다!!!");
+  //   // const result = localStorage.getItem("accessToken");
+  //   // console.log(result);
+  //   // if (result) setAccessToken(result);
+  // } else {
+  //   // console.log("지금은 프론트엔드 서버다!!!!!(즉, tarn dev로 실행시킨 프로그램 내부다!!!)");
+  //   // const result = localStorage.getItem("accessToken");
+  //   // console.log(result);
+  //   // if (result) setAccessToken(result);
+  // }
+
+  // 2. 프리렌더링 예제 - typeof window 방법
+  // if (typeof window !== "undefined") {
+  //   // console.log("지금은 브라우저다!!!");
+  //   // const result = localStorage.getItem("accessToken");
+  //   // console.log(result);
+  //   // if (result) setAccessToken(result);
+  // } else {
+  //   // console.log("지금은 프론트엔드 서버다!!!!!(즉, tarn dev로 실행시킨 프로그램 내부다!!!)");
+  //   // const result = localStorage.getItem("accessToken");
+  //   // console.log(result);
+  //   // if (result) setAccessToken(result);
+  // }
+
+  // 3. 프리렌더링 무시 - useEffect 방법
+  useEffect(() => {
+    console.log("지금은 브라우저다!!!");
+    const result = localStorage.getItem("accessToken");
+    console.log(result);
+    if (result) setAccessToken(result);
+  }, []);
 
   const uploadLink = createUploadLink({
     uri: "http://backendonline.codebootcamp.co.kr/graphql",
