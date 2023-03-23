c38ded46a7ded9434d537386e9cd01a4442a83bf
ncb6206 <ncb6206@gmail.com>
2023-03-23 17:15:00 +0900

    section.19 수강완료

--- class/src/components/commons/apollo/index.tsx
+++ class/src/components/commons/apollo/index.tsx
@@ -1,8 +1,18 @@
-import { ApolloProvider, ApolloClient, InMemoryCache, ApolloLink } from "@apollo/client";
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  ApolloLink,
+  gql,
+  fromPromise,
+} from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
 import { createUploadLink } from "apollo-upload-client";
 import { useEffect } from "react";
 import { useRecoilState } from "recoil";
 import { accessTokenState } from "../../../commons/store";
+
+import { getAccessToken } from "../../../commons/libraries/getAccessToken";
 
 interface IApolloSettingProps {
   children: JSX.Element;
@@ -41,19 +51,53 @@ export default function ApolloSetting(props: IApolloSettingProps) {
 
   // 3. 프리렌더링 무시 - useEffect 방법
   useEffect(() => {
-    console.log("지금은 브라우저다!!!");
-    const result = localStorage.getItem("accessToken");
-    console.log(result);
-    if (result) setAccessToken(result);
+    // 1. 기존방식(refreshToken 이전)
+    // console.log("지금은 브라우저다!!!");
+    // const result = localStorage.getItem("accessToken");
+    // console.log(result);
+    // if (result) setAccessToken(result);
+
+    // 2. 새로운방식(refreshToken 이후)
+    void getAccessToken().then((newAccessToken) => {
+      setAccessToken(newAccessToken);
+    });
   }, []);
+
+  const errorLink = onError(({ graphQLErrors, operation, forward }) => {
+    // 1-1. 에러를 캐치
+    if (graphQLErrors) {
+      for (const err of graphQLErrors) {
+        // 1-2. 해당 에러가 토큰만료 에러인지 체크(UNAUTHENTICATED)
+        if (err.extensions.code === "UNAUTHENTICATED") {
+          return fromPromise(
+            // 2-1. refreshToken으로 accessToken을 재발급 받기
+            getAccessToken().then((newAccessToken) => {
+              // 2-2. 재발급 받은 accessToken 저장하기
+              setAccessToken(newAccessToken);
+
+              // 3-1. 재발급 받은 accessToken으로 방금 실패한 쿼리의 정보 수정하기
+              if (typeof newAccessToken !== "string") return;
+              operation.setContext({
+                headers: {
+                  ...operation.getContext().headers, // 만료된 토큰이 추가되어 있는 상태
+                  Authorization: `Bearer ${newAccessToken}`, // 토큰만 새걸로 바꿔치기
+                },
+              });
+            })
+          ).flatMap(() => forward(operation)); // 3-2. 방금 수정한 쿼리 재요청하기
+        }
+      }
+    }
+  });
 
   const uploadLink = createUploadLink({
     uri: "https://backendonline.codebootcamp.co.kr/graphql",
     headers: { Authorization: `Bearer ${accessToken}` },
+    credentials: "include",
   });
 
   const client = new ApolloClient({
-    link: ApolloLink.from([uploadLink]),
+    link: ApolloLink.from([errorLink, uploadLink]),
     cache: GLOBAL_STATE, // 페이지 전환{_app.tsx 리렌더} 되어도, 캐시 유지
     connectToDevTools: true,
   });
