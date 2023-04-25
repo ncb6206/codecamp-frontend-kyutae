e6d7fddce8efc8eba14051949cfad572e9273dd2
ncb6206 <ncb6206@gmail.com>
2023-04-26 00:33:29 +0900

    동적페이지 업데이트

--- class_build/pages/boards/[boardId]/index.tsx
+++ class_build/pages/boards/[boardId]/index.tsx
@@ -1,3 +1,12 @@
+import { useRouter } from "next/router";
+
 export default function BoardsPage() {
-  return <div>안녕하세요 게시판입니다~ 동적페이지예요!</div>;
+  const router = useRouter();
+
+  return (
+    <div>
+      안녕하세요 게시판입니다~ 동적페이지예요! <br />
+      게시글아이디: {router.query.boardId}
+    </div>
+  );
 }
